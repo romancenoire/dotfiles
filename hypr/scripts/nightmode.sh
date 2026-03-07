3e53bb2bb9d45cd4a39100843720d29de4e86781
romancenoire <68818499+romancenoire@users.noreply.github.com>
2026-03-07 01:19:55 +0100

    Fixing nightmode + reducing gamma by 25% by default

--- hypr/scripts/nightmode.sh
+++ hypr/scripts/nightmode.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 TEMP="${HYPRSUNSET_TEMP:-4000}"
+GAMMA="${HYPRSUNSET_GAMMA:-75}"
 STATE_FILE="/tmp/hyprsunset_state"
 
 if [ "$(cat $STATE_FILE 2>/dev/null)" = "on" ]; then
-    hyprctl hyprsunset identity
+    pkill hyprsunset
     echo "off" > "$STATE_FILE"
     notify-send "Night Light" "Off" -u low -i weather-clear
 else
-    hyprctl hyprsunset temperature "$TEMP"
+    hyprsunset -t "$TEMP" -g "$GAMMA" &
     echo "on" > "$STATE_FILE"
-    notify-send "Night Light" "On (${TEMP}K)" -u low -i weather-clear-night
+    notify-send "Night Light" "On (${TEMP}K - ${GAMMA}%)" -u low -i weather-clear-night
 fi

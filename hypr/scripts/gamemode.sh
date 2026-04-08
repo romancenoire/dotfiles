ee994d7453c56afee3a8afb90a19a910005a66a4
romancenoire <68818499+romancenoire@users.noreply.github.com>
2026-04-09 00:57:21 +0200

    Adding support for Matugen, Adding keybinds for muting mic, tweaking few rules and env vars

--- hypr/scripts/gamemode.sh
+++ hypr/scripts/gamemode.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
 HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
-
 if [ "$HYPRGAMEMODE" = 1 ] ; then
     pkill waybar
-
     hyprctl --batch "\
         keyword animations:enabled 0;\
         keyword decoration:shadow:enabled 0;\
@@ -13,10 +10,9 @@ if [ "$HYPRGAMEMODE" = 1 ] ; then
         keyword decoration:inactive_opacity 1;\
         keyword decoration:fullscreen_opacity 1;\
         keyword general:gaps_in 0;\
-	    keyword general:gaps_out 0;\
+        keyword general:gaps_out 0;\
         keyword general:border_size 1;\
         keyword decoration:rounding 0"
-
     notify-send -t 1000 -u normal -i "input-gaming" "Gamemode" "Activé (Animations et Waybar OFF)"
     exit
 else
